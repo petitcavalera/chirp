3b22cd3ebe9b9066d2828dec15cb7346bb8f6dfb
mrifkysaifulhuda <mrifkysaifulhuda@gmail.com>
2016-03-24 09:57:57 +0700

    Edit app.js

--- app.js
+++ app.js
@@ -4,71 +4,87 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+
 var passport = require('passport');
+var session = require('express-session');
+
+var app = express();
+
 //initialize mongoose schemas
 require('./models/models');
-var index = require('./routes/index');
+var mongoose = require('mongoose');                         //add for Mongo support
+
+//connect to Mongo
+if (app.get('env') === 'production') {
+    mongoose.connect('mongodb://test:test@ds064718.mlab.com:64718/test-chirp'); 
+}
+else{
+    mongoose.connect('mongodb://localhost/test-chirp');             
+}
+
 var api = require('./routes/api');
 var authenticate = require('./routes/authenticate')(passport);
-var mongoose = require('mongoose');                         //add for Mongo support
-mongoose.connect('mongodb://testUser:testPassword@ds021989.mlab.com:21989/test-chirp');              //connect to Mongo
-var app = express();
+var adminuser = require('./routes/adminuser');
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(session({
   secret: 'keyboard cat'
 }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use('/', index);
-app.use('/auth', authenticate);
-app.use('/api', api);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 //// Initialize Passport
 var initPassport = require('./passport-init');
 initPassport(passport);
+
+app.use('/auth', authenticate);
+app.use('/api', api);
+app.use('/user', adminuser);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
